192a60af9a6ad0f1c7dcd0f2dc3910fd3620d75c
praveen <work.developer20211@gmail.com>
2022-12-02 15:12:35 +0530

    '360image'

--- application/modules/admin/views/showrooms/images-add.php
+++ application/modules/admin/views/showrooms/images-add.php
@@ -38,7 +38,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                       <div class="col-md-6">
                           <label for="inputName1" class="col-sm-4 control-label">
                           Retailer </label>
-                          <input type="text" name="retailer"  class="form-control" id="inputName1" multiple  / >
+                          <input type="text" name="retailer"  class="form-control" id="inputName1"   / >
                       </div>
 
                       <div class="col-md-6">
@@ -58,13 +58,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                        <div class="row showcod1">
                           <div class="col-sm-5">
                             <input type="hidden" name="codeno0[]" value="0" >
-                              X : <input type="number" step="0.00000001"  name="xval0" style="width:20%">
-                              Y : <input type="number" step="0.00000001"  name="yval0" style="width:20%">
-                              Z : <input type="number" step="0.00000001"  name="zval0" style="width:20%">
+                              X : <input type="number" step="0.00000001"  name="xval0" style="width:20%" required>
+                              Y : <input type="number" step="0.00000001"  name="yval0" style="width:20%" required>
+                              Z : <input type="number" step="0.00000001"  name="zval0" style="width:20%" required>
                           </div>
 
                           <div class="col-sm-4">
-                              <textarea  name="coordinate_360_info0" class="form-control" placeholder="Info text"></textarea>
+                              <textarea  name="coordinate_360_info0" required class="form-control" placeholder="Info text"></textarea>
                           </div>
                           <div class="col-md-3">
                               <label for="inputName1" class="control-label">Images</label>
@@ -123,7 +123,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 var noc = 1;
 function add_coodi(no){
   
-     $(".showcod"+no).after('<div style="padding:15px 0px" class="row showcod'+ noc +' removecla'+ noc +'"><input type="hidden" name="nos360[]" value="'+ noc +'"><div class="col-sm-5"> X : <input type="number" step="0.00000001" name="xval'+ noc +'" style="width:20%"> Y : <input type="number" step="0.00000001" name="yval'+ noc +'" style="width:20%"> Z : <input type="number" step="0.00000001" name="zval'+ noc +'" style="width:20%"></div><div class="col-sm-4"><textarea style="width: 1200px;" name="coordinate_360_info'+ noc +'" class="form-control" placeholder="Info text"></textarea></div><div class="col-md-3"> <label for="inputName1" class="control-label">Images</label> <input type="file" style="width:247px" name="image'+ noc +'[]" class="form-control" id="inputName1" multiple accept="image/x-png,image/gif,image/jpeg" / > </div> <div id="education_fields'+ noc +'"> <div class="row"> <div class="col-md-4"> <label for="inputName1" class="control-label">3D Modals</label> <input type="file" style="width:247px" name="3dmodals'+ noc +'" class="form-control" id="inputName1"  / > </div> <div class="col-md-4"> <label for="inputName1" class="control-label">Modals Color</label> <input type="text" style="width:247px" name="modals_color'+ noc +'" class="form-control" id="inputName1"  / > </div> <div class="col-md-4"> <button style="margin: 30px 21px; float: left;" class="btn btn-success" type="button" onclick="education_fields('+ noc +');">+</button> </div> </div> </div>  <div class="col-sm-12" style="text-align:right"><button class="btn btn-danger" type="button" onclick="remove_education_fields2('+ noc +');"> Remove</button><div></div>'); 
+     $(".showcod"+no).after('<div style="padding:15px 0px" class="row showcod'+ noc +' removecla'+ noc +'"><input type="hidden" name="nos360[]" value="'+ noc +'"><div class="col-sm-5"> X : <input type="number" required step="0.00000001" name="xval'+ noc +'" style="width:20%"> Y : <input type="number" required step="0.00000001" name="yval'+ noc +'" style="width:20%"> Z : <input type="number" required step="0.00000001" name="zval'+ noc +'" style="width:20%"></div><div class="col-sm-4"><textarea required style="width: 1200px;" name="coordinate_360_info'+ noc +'" class="form-control" placeholder="Info text"></textarea></div><div class="col-md-3"> <label for="inputName1" class="control-label">Images</label> <input type="file" style="width:247px" name="image'+ noc +'[]" class="form-control" id="inputName1" multiple accept="image/x-png,image/gif,image/jpeg" / > </div> <div id="education_fields'+ noc +'"> <div class="row"> <div class="col-md-4"> <label for="inputName1" class="control-label">3D Modals</label> <input type="file" style="width:247px" name="3dmodals'+ noc +'" class="form-control" id="inputName1"  / > </div> <div class="col-md-4"> <label for="inputName1" class="control-label">Modals Color</label> <input type="text" style="width:247px" name="modals_color'+ noc +'" class="form-control" id="inputName1"  / > </div> <div class="col-md-4"> <button style="margin: 30px 21px; float: left;" class="btn btn-success" type="button" onclick="education_fields('+ noc +');">+</button> </div> </div> </div>  <div class="col-sm-12" style="text-align:right"><button class="btn btn-danger" type="button" onclick="remove_education_fields2('+ noc +');"> Remove</button><div></div>'); 
       noc++;
 
 }
